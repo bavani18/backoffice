c36a2528317647c09d995750a312a72843af8beb
bavani18 <bavaniabavani18@gmail.com>
2026-03-25 09:32:02 +0530

    fix: add yaml + sync lock

--- backend/server.js
+++ backend/server.js
@@ -1423,6 +1423,10 @@ app.delete("/modifiermaster/:id", async (req, res) => {
   }
 });
 /* ------------------- SERVER ------------------- */
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
