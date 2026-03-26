1ad3fb973699274710566de64d0260a51603bb7b
bavani18 <bavaniabavani18@gmail.com>
2026-03-26 11:28:10 +0530

    fix db timeout

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
-
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
